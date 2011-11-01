d699b4c9979bbddc9edcc08fb7fe1e9647d0d5c4
booger4096 <booger4096@gmail.com>
2011-11-01 05:22:01 +0400

    After all again server commit

---
+++ EchoComponent/EchoClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using EchoClient.ServiceReference;
+
+namespace EchoClient {
+  class Program {
+    static void Main(string[] args) {
+      EchoServerClient proxy = new EchoServerClient();
+      string s = "EchoClient";
+      Console.WriteLine(s);
+      Console.WriteLine(proxy.Echo(s));
+      Console.ReadLine();
+    }
+  }
+}
