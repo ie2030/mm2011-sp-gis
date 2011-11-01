65a795fc535ff8aea3e0261d1d93fd279c22113d
booger4096 <booger4096@gmail.com>
2011-11-01 04:23:00 +0400

    Server part
    IComponent is interface for all components
    EchoComponent is example component which starts simple WCF Service
    
    Not sure what everything will work
    Sorry for bad english

---
+++ EchoComponent/EchoComponent/EchoComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.ServiceModel;
+using Server;
+
+namespace EchoComponent {
+
+  public class EchoServer:IEchoServer {
+    // Simple WCF Service
+    public string Echo(string value) {
+      return "got: " + value;
+    }
+  }
+
+  public class EchoComponent: IComponent  {
+    // Component which starts EchoServer
+    public void start() {
+      ServiceHost host = new ServiceHost(typeof(EchoServer));
+      host.Open();
+    }
+
+  }
+}
---
+++ EchoComponent/EchoComponent/IEchoServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace EchoComponent {
+  [ServiceContract]
+  public interface IEchoServer {
+    [OperationContract]
+    string Echo(string value);
+  }
+}
