1f1099a87a64e7ab35d34bb0a836df70600dd903
Artur Baykov <arturbaykov@gmail.com>
2011-11-29 15:03:24 +0400

    Добавлен интерфейс к классу GraphForAlgorithm.cs
    Signed-off-by: Artur Baykov <arturbaykov@gmail.com>

--- SearchAlgorithm/SearchAlgorithm/GraphForAlgorithm.cs
+++ SearchAlgorithm/SearchAlgorithm/GraphForAlgorithm.cs
@@ -5,7 +5,34 @@ using System.Text;
 
 namespace Server
 {
-    class GraphForAlgorithm
+    public interface IGraphForAlgorithm
     {
+        OnDataBase[] GiveMeIdPriorCrossroads(int id);
+        OnDataBase[] GiveMeId(int id);
+    }
+
+
+
+    public enum PriorityVertex
+    {
+        Crossroads,
+        Home
+    }
+
+    class OnDataBase
+    {
+        int id1;
+        int id2;
+        PointCoordinates id2Coordinates;
+        PriorityVertex id2Priority; 
+        TimeSpan time;
+    }
+
+    class GraphForAlgorithm : IGraphForAlgorithm
+    {
+
     }
 }
+
+
+// Id1 Id2 time track prior koordinatid1 
