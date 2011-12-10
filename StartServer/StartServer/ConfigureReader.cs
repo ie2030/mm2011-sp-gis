6316c0111a70d90866fe9d8a39cbb267f1a69f27
Iskander <monpak@mail.ru>
2011-12-11 01:33:33 +0400

    Some refactoring for the Start server. Use this codestyle as an example

---
+++ StartServer/StartServer/ConfigureReader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Xml;
+using Server;
+
+namespace Server
+{
+    /// <summary>
+    /// Incapsulates the logic for getting the server components from config
+    /// </summary>
+    internal class ConfigureReader : IConfigurationSectionHandler
+    {
+        #region public methods
+        /// <summary>
+        /// Creates an object using context section
+        /// </summary>
+        public object Create(object parent, object configContext, XmlNode section)
+        {
+            List<IComponent> components = new List<IComponent>();
+            foreach (XmlNode node in section.ChildNodes)
+                if (node.Name == "component")
+                    try
+                    {
+                        object curr = Activator.CreateInstance(Type.GetType(node.Attributes["type"].Value));
+                        IComponent component = (IComponent)curr;
+                        components.Add(component);
+                    }
+                    catch (ArgumentNullException)
+                    {
+                        Console.WriteLine("Error: Component '" + node.Attributes["type"].Value + "' is not found");
+                        continue;
+                    }
+                    catch (NullReferenceException)
+                    {
+                        Console.WriteLine("Error: Attribute 'type' is not found");
+                        continue;
+                    }
+                    catch (InvalidCastException)
+                    {
+                        Console.WriteLine("Error: " + node.Attributes["type"].Value + " don't implements IComponent");
+                        continue;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Unknown Error");
+                        continue;
+                    }
+            return components;
+        }
+        #endregion
+    }
+}
--- StartServer/StartServer/StartServer.cs
+++ StartServer/StartServer/StartServer.cs
@@ -3,54 +3,36 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Xml;
 
-namespace Server {
-
-  class ConfigureReader:IConfigurationSectionHandler {
-    // Class which gets all components from app.config
-    public object Create(object parent, object configContext, XmlNode section) {
-      List<IComponent> components = new List<IComponent>();
-      foreach (XmlNode node in section.ChildNodes)
-        if (node.Name == "component")
-          try {
-            object curr = Activator.CreateInstance(Type.GetType(node.Attributes["type"].Value));
-            IComponent component = (IComponent)curr;
-            components.Add(component);
-          }
-          catch (ArgumentNullException) {
-            Console.WriteLine("Error: Component '" + node.Attributes["type"].Value + "' is not found");
-            continue;
-          }
-          catch (NullReferenceException) {
-            Console.WriteLine("Error: Attribute 'type' is not found");
-            continue;
-          }
-          catch (InvalidCastException) {
-            Console.WriteLine("Error: " + node.Attributes["type"].Value + " don't implements IComponent");
-            continue;
-          }
-          catch {
-            Console.WriteLine("Unknown Error");
-            continue;
-          }
-      return components;
-    }
-  }
-
-  class StartServer {
-    static void Main(string[] args) {
-      List<IComponent> components = (List<IComponent>)ConfigurationSettings.GetConfig("components");
-      foreach (IComponent component in components) {
-        try {
-          component.start();
+namespace Server
+{
+    /// <summary>
+    /// Start the server. Holds the entry point
+    /// </summary>
+    public class StartServer
+    {
+        #region public methods
+        /// <summary>
+        /// Entry point of the application
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            List<IComponent> components = (List<IComponent>)ConfigurationSettings.GetConfig("components");
+            foreach (IComponent component in components)
+            {
+                try
+                {
+                    component.start();
+                }
+                catch
+                {
+                    Console.WriteLine("Error while starting " + component.GetType());
+                    continue;
+                }
+                Console.WriteLine(component.GetType() + " is started");
+            }
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
         }
-        catch {
-          Console.WriteLine("Error while starting " + component.GetType());
-          continue;
-        }
-        Console.WriteLine(component.GetType() + " is started");
-      }
-      Console.WriteLine("Press Enter to exit");
-      Console.ReadLine();
+        #endregion
     }
-  }
 }
