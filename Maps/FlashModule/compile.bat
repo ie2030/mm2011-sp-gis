ea6b1f281d8897623231e94dbebd7d313e0adb18
booger4096 <booger4096@gmail.com>
2012-04-18 18:47:24 +0400

    Simple client Located in Maps/GoogleFlashInWpf

---
+++ Maps/FlashModule/compile.bat
@@ -0,0 +1,4 @@
+@echo off
+mxmlc GoogleMaps.mxml -library-path+=map_flex_1_20.swc  > log.txt 2>&1
+:ololo
+goto ololo
--- Maps/GoogleMapsFlashInWpf/GoogleMapsFlashInWpf/MainWindow.xaml.cs
+++ Maps/GoogleMapsFlashInWpf/GoogleMapsFlashInWpf/MainWindow.xaml.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Linq;
-using System.Xml.Linq;
-using System.Windows;
-using System.Windows.Input;
+﻿using System.Windows;
+using System.Collections.Generic;
 using System.Collections;
 using Flash.External;
 
@@ -11,16 +8,19 @@ namespace GoogleMapsFlashInWpf
     ///
     /// Interaction logic for Window1.xaml
     ///
-    public partial class Window1 : Window
+    public partial class Window1 :Window
     {
 
         private ExternalInterfaceProxy proxy;
-        
+        private double stLat, stLng, fnLat, fnLng;
+        private Services.AlgorithmServerClient algorithm;
+
         public Window1()
         {
             InitializeComponent();
+            algorithm = new Services.AlgorithmServerClient();
         }
-        
+
         private void WindowLoaded(object sender, RoutedEventArgs e)
         {
             axFlash.FlashVars = "ABQIAAAA6Je9wEhpps-6h4SLEzfx0hQcnJAsAPU0edVn7hFTQC8ea3A_VBRkCS6mZzjo_25VBG1y_bIKsYiRMg";
@@ -28,72 +28,90 @@ namespace GoogleMapsFlashInWpf
 
             proxy = new ExternalInterfaceProxy(axFlash);
             proxy.ExternalInterfaceCall += new ExternalInterfaceCallEventHandler(proxy_ExternalInterfaceCall);
+
+            stButton.IsEnabled = true;
+            fnButton.IsEnabled = true;
         }
-        
+
         /// <summary>
         /// Parse calls from flash application
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        /// <returns></returns>
-        private object proxy_ExternalInterfaceCall(object sender, ExternalInterfaceCallEventArgs e) {
-          switch (e.FunctionCall.FunctionName) {
-            case "setPosition":
-              setPosition(e.FunctionCall.Arguments[0].ToString(), e.FunctionCall.Arguments[1].ToString());
-              return null;
-          }
-          return null;
-        }
-
-        private void setPosition(string newLat, string newLng) 
+        private object proxy_ExternalInterfaceCall(object sender, ExternalInterfaceCallEventArgs e)
         {
-          lat.Text = newLat;
-          lng.Text = newLng;
+            switch (e.FunctionCall.FunctionName)
+            {
+                case "setStPosition":
+                    setStPosition((double)e.FunctionCall.Arguments[0], (double)e.FunctionCall.Arguments[1]);
+                    return null;
+                case "setFnPosition":
+                    setFnPosition((double)e.FunctionCall.Arguments[0], (double)e.FunctionCall.Arguments[1]);
+                    return null;
+            }
+            return null;
         }
 
-        private void TextBox_KeyDown(object sender, KeyEventArgs e)
-        {   
-            if (e.Key == Key.Enter)
-                Search_Click(sender, null);
-        }
-
-        private void Search_Click(object sender, RoutedEventArgs e)
+        private void setStPosition(double lat, double lng)
         {
-            if (address.Text != "")
-              proxy.Call("Search", address.Text);
+            stLat = lat;
+            stLng = lng;
         }
+
+        private void setFnPosition(double lat, double lng)
+        {
+            fnLat = lat;
+            fnLng = lng;
+        }
+
         /// <summary>
         /// Draw polyline on Map
         /// </summary>
         /// <param name="points">ArrayList of points. Each elem is ArrayList with lat and lng</param>
-        private void drawLine(ArrayList points) {
-            proxy.Call("Drawline", points);
-        }
-        
-        /// <summary>
-        /// Delete polyline from Map
-        /// </summary>
-        private void clearLine() {
-            proxy.Call("Clearline");
-        }
-        
-        // Example of using drawLine
-        private void paintSomeLine(object sender, RoutedEventArgs e) {
-          ArrayList points = new ArrayList();
-          double x = Convert.ToDouble(lat.Text); //Current marker coordinats
-          double y = Convert.ToDouble(lng.Text);
-          points.Add(new ArrayList(new double[2]{x, y}));
-          x -= 0.001;
-          y += 0.001;
-          points.Add(new ArrayList(new double[2]{x, y }));
-          x += 0.0003;
-          y -= 0.0015;
-          points.Add(new ArrayList(new double[2]{x, y }));
-          drawLine(points);
+        private void drawLine(ArrayList points)
+        {
+            if (proxy != null)
+                proxy.Call("Drawline", points);
         }
 
-        private void Clear_Click(object sender, RoutedEventArgs e) {
-          clearLine();
+        private void clear()
+        {
+            if (proxy != null)
+                proxy.Call("Clear");
         }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            clear();
+        }
+
+        private void stButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (proxy != null)
+                proxy.Call("SelectMarker", 0);
+        }
+
+        private void fnButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (proxy != null) 
+                proxy.Call("SelectMarker", 1);
+        }
+
+
+        private void get_Path_Click(object sender, RoutedEventArgs e)
+        {
+            Services.Node start = new Services.Node();
+            start.lat = stLat;
+            start.lon = stLng;
+            Services.Node finish = new Services.Node();
+            finish.lat = fnLat;
+            finish.lon = fnLng;
+            Services.Node[] path = algorithm.getShortestPath(start, finish);
+            Title = path.Length.ToString();
+            ArrayList points = new ArrayList();
+            foreach (Services.Node point in path){
+                points.Add(new ArrayList(new double[2] {point.lat, point.lon}));
+            }
+            drawLine(points);
+        }
+
     }
 }
