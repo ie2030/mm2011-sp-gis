bae6710edc2da1b87dff772d315147dc632f480d
Marie <leesbonbons@gmail.com>
2012-08-07 18:57:36 +0400

    Проект

---
+++ Desktop/проект авто - копия/StartServer/StartServer/StartServer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace Server
+{
+    /// <summary>
+    /// Start the server. Holds the entry point
+    /// </summary>
+    public class StartServer
+    {
+        #region public methods
+        /// <summary>
+        /// Entry point of the application
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            List<IComponent> components = (List<IComponent>)ConfigurationSettings.GetConfig("components");
+            foreach (IComponent component in components)
+            {
+                try
+                {
+                    component.start();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error while starting " + component.GetType());
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                Console.WriteLine(component.GetType() + " is started");
+            }
+
+            
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+        }
+        #endregion
+    }
+}
